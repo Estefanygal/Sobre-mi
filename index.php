fdfffa421c87686b694f8f8ca493875151aff7a7
Estefanygal <estefanygalvanmoncada85@gmail.com>
2024-08-13 18:49:17 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'header.php'; ?>
+    <h1>Bienvenido a mi sitio web personal</h1>
+    <p>Hola, soy Estefany Galván Moncada. Soy desarrolladora web con experiencia en tecnologías frontend y backend. ¡Explora mis proyectos y conoce más sobre mí!</p>
+<?php include 'footer.php'; ?>
