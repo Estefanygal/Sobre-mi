35ede16ba61bb3e48a7c25f033a0ff9c785e66ba
Estefanygal <estefanygalvanmoncada85@gmail.com>
2024-08-13 18:49:49 -0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,4 @@
+<?php include 'header.php'; ?>
+    <h1>Sobre mí</h1>
+    <p>Soy una desarrolladora web apasionada por crear aplicaciones web innovadoras. Tengo experiencia en varios lenguajes de programación, incluyendo PHP, JavaScript, y Python.</p>
+<?php include 'footer.php'; ?>
