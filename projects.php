35e15fa1a69d45f3e4c9c2f89ded7e5f13947013
Estefanygal <estefanygalvanmoncada85@gmail.com>
2024-08-14 01:21:31 -0600

    Actualizar projects.php

--- projects.php
+++ projects.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
     <h1>Mis Proyectos</h1>
     <ul>
-        <li><strong>Proyecto 1:</strong> Descripción del proyecto 1.</li>
+        <li><strong>Proyecto 1:</strong> Coucherick Canguro Store es una plataforma de comercio electrónico diseñada para ofrecer una experiencia de compra fluida y accesible. Esta página web está orientada a la venta de productos especializados, con un enfoque en la comodidad y satisfacción del usuario. La interfaz ha sido optimizada para ser intuitiva, permitiendo a los clientes navegar y realizar compras de manera sencilla y segura.</li>
         <li><strong>Proyecto 2:</strong> Descripción del proyecto 2.</li>
         <li><strong>Proyecto 3:</strong> Descripción del proyecto 3.</li>
         <!-- Añade más proyectos aquí -->
