8229fba8c5598daff8469baf587671cf6a122883
Estefanygal <estefanygalvanmoncada85@gmail.com>
2024-08-14 01:24:05 -0600

    Actualizar projects.php

--- projects.php
+++ projects.php
@@ -2,8 +2,7 @@
     <h1>Mis Proyectos</h1>
     <ul>
         <li><strong>Proyecto 1:</strong> Coucherick Canguro Store es una plataforma de comercio electrónico diseñada para ofrecer una experiencia de compra fluida y accesible. Esta página web está orientada a la venta de productos especializados, con un enfoque en la comodidad y satisfacción del usuario. La interfaz ha sido optimizada para ser intuitiva, permitiendo a los clientes navegar y realizar compras de manera sencilla y segura.</li>
-        <li><strong>Proyecto 2:</strong> Descripción del proyecto 2.</li>
-        <li><strong>Proyecto 3:</strong> Descripción del proyecto 3.</li>
-        <!-- Añade más proyectos aquí -->
+        <li><strong>Proyecto 2:</strong> Mex-Link es una plataforma de comercio digital enfocada en conectar a usuarios con una amplia gama de productos y servicios de calidad en México. La página está diseñada para ser el puente entre consumidores y negocios, facilitando un acceso rápido y eficiente a una diversa oferta comercial. La estructura de la web es moderna y está optimizada para asegurar una navegación fluida, ofreciendo a los usuarios una experiencia agradable y efectiva.</li>
+        
     </ul>
 <?php include 'footer.php'; ?>
