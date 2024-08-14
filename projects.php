c9e4a2ee9ab8a2e72abab36e2186fd1f8857961e
Estefanygal <estefanygalvanmoncada85@gmail.com>
2024-08-13 18:50:23 -0600

    Create projects.php

---
+++ projects.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+    <h1>Mis Proyectos</h1>
+    <ul>
+        <li><strong>Proyecto 1:</strong> Descripción del proyecto 1.</li>
+        <li><strong>Proyecto 2:</strong> Descripción del proyecto 2.</li>
+        <li><strong>Proyecto 3:</strong> Descripción del proyecto 3.</li>
+        <!-- Añade más proyectos aquí -->
+    </ul>
+<?php include 'footer.php'; ?>
